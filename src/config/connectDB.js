90da8986d8fbfd678113c17115a82731ebeb28a3
chiennguyen30 <nguyenchien30102001@gmail.com>
2024-06-14 22:08:12 +0700

    df

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,9 +2,13 @@ const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize("nvcit", "root", null, {
-  host: "localhost",
+  host: "127.0.0.1", // or host: 'localhost'
   dialect: "mysql",
+  port: 3306, // your port
   logging: false,
+  dialectOptions: {
+    socketPath: "C:/xampp/mysql/mysql.sock",
+  },
 });
 
 let connectDB = async () => {
