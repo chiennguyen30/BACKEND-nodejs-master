e453fc6985d3dc2144df0b1b36dcbc1655e7d186
chiennguyen30 <nguyenchien30102001@gmail.com>
2024-06-15 15:52:59 +0700

    dsa

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+const sequelize = new Sequelize("nvcit", "root", null, {
+  host: "127.0.0.1",
+  port: 3306,
   dialect: "mysql",
   logging: false,
 });
